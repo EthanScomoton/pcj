1836a369f9e9c3c56e707bdff36acd2b69c7f4eb
EthanScomoton <zhuhe2510@gmail.com>
2025-07-15 14:59:53 +0800

    update model 5 256

--- All_Models/All_Models_EGrid_PaperV20.py
+++ All_Models/All_Models_EGrid_PaperV20.py
@@ -793,7 +793,7 @@ class EModel_FeatureWeight5(nn.Module):
     def __init__(self,
                  feature_dim,
                  window_size=window_size,          # 序列窗口长度
-                 lstm_hidden_size=128,
+                 lstm_hidden_size=256,
                  lstm_num_layers=2,
                  lstm_dropout=0.1, 
                 ):
@@ -1635,7 +1635,7 @@ def main(use_log_transform = True, min_egrid_threshold = 1.0):
 
     model5 = EModel_FeatureWeight5(
         feature_dim       = feature_dim,
-        lstm_hidden_size  = 128, 
+        lstm_hidden_size  = 256, 
         lstm_num_layers   = 2,
         lstm_dropout      = 0.1
     ).to(device)
@@ -1728,7 +1728,7 @@ def main(use_log_transform = True, min_egrid_threshold = 1.0):
 
     best_model5 = EModel_FeatureWeight5(
         feature_dim       = feature_dim,
-        lstm_hidden_size  = 128, 
+        lstm_hidden_size  = 256, 
         lstm_num_layers   = 2
     ).to(device)
     best_model5.load_state_dict(torch.load('best_EModel_FeatureWeight5.pth', map_location=device, weights_only=True), strict=False)
